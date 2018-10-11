c82f637d7a449d52726132f091c412fe3dae21e9
João Reys Santos <je_reyssantos@msn.com>
2018-10-11 15:46:16 +0100

    Adding book types table

---
+++ database/migrations/2018_10_11_140815_create_book_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() : void
+    {
+        Schema::create('book_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() : void
+    {
+        Schema::dropIfExists('book_types');
+    }
+}
