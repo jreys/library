5a2622854544da867601ce6d173d7fd8744c2e38
João Reys Santos <je_reyssantos@msn.com>
2018-10-11 15:46:34 +0100

    Adding main books table with foreign keys

---
+++ database/migrations/2018_10_11_141304_create_books_table.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * @var string - Datatable's name.
+     */
+    const TABLE_NAME = 'books';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() : void
+    {
+        Schema::create(self::TABLE_NAME, function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->integer('pages');
+            $table->timestamps();
+
+            // Adding foreign keys
+            $table->unsignedInteger('author')
+                ->nullable();
+
+            $table->unsignedInteger('book_type')
+                ->nullable();
+
+            // Adding foreing key references
+            $table->foreign('author', self::TABLE_NAME . "_author_foreign")
+                ->references('id')
+                ->on('authors')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+
+            $table->foreign("book_type", self::TABLE_NAME . "_book_type_foreign")
+                ->references('id')
+                ->on('book_types')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() : void
+    {
+        // Removing foreign key references
+        Schema::table(self::TABLE_NAME, function (Blueprint $table) {
+            $table->dropForeign(self::TABLE_NAME . '_author_foreign');
+            $table->dropForeign(self::TABLE_NAME . '_book_type_foreign');
+        });
+
+        Schema::dropIfExists('books');
+    }
+}
