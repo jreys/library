3d7e5bbdd288727a8a6af94f2deb5a5544d90b94
João Reys Santos <je_reyssantos@msn.com>
2018-10-15 16:20:41 +0100

    Code style

--- resources/views/books/add.blade.php
+++ resources/views/books/add.blade.php
@@ -6,7 +6,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card"><div class="card-header">Add a New Book</div>
+                <div class="card">
+                    <div class="card-header">Add a New Book</div>
                     <div class="card-body">
                         <form method="POST" action="/books/store">
                             {{ csrf_field() }}
