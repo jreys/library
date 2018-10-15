9b4fa1edbe15407bda775831e8edb42113c72d72
João Reys Santos <je_reyssantos@msn.com>
2018-10-15 15:09:50 +0100

    Adding generic delete button with modal

---
+++ resources/views/layouts/delete.blade.php
@@ -0,0 +1,23 @@
+<button type="button" class="btn btn-danger ml-auto" data-toggle="modal" data-target="#deleteModal">
+    Delete
+</button>
+
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModal">Alert!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to delete this?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <a href="{{ $url }}" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+    </div>
+</div>
