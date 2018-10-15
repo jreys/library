092a08b287ef52b854c48f9ca799b5a5bc0ffcd4
João Reys Santos <je_reyssantos@msn.com>
2018-10-15 18:28:14 +0100

    Adding Update routes

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,21 @@ Route::get('/', function () {
 // Author related routes
 Route::get('/authors', 'AuthorsController@list');
 Route::get('/authors/add', 'AuthorsController@add');
-Route::get('/authors/delete/{$id}', 'AuthorsController@delete');
+Route::get('/authors/update/{id}', 'AuthorsController@update');
+Route::get('/authors/delete/{id}', 'AuthorsController@delete');
 Route::post('/authors/store', 'AuthorsController@store');
 
 // Book types related routes
 Route::get('/booktypes', 'BookTypesController@list');
 Route::get('/booktypes/add', 'BookTypesController@add');
+Route::get('/booktypes/update/{id}', 'BookTypesController@update');
 Route::get('/booktypes/delete/{id}', 'BookTypesController@delete');
 Route::post('/booktypes/store', 'BookTypesController@store');
 
 // Books related routes
 Route::get('/books', 'BooksController@list');
 Route::get('/books/add', 'BooksController@add');
+Route::get('/books/update/{id}', 'BooksController@update');
 Route::get('/books/delete/{id}', 'BooksController@delete');
 Route::post('/books/store', 'BooksController@store');
 
