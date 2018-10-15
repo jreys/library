92f829d3350288187857bc67cd2de0fd2d0d5623
João Reys Santos <je_reyssantos@msn.com>
2018-10-15 16:19:51 +0100

    Adding list view URLs to main menu

--- resources/views/layouts/left-menu.blade.php
+++ resources/views/layouts/left-menu.blade.php
@@ -1,4 +1,4 @@
-<li class="nav-item">
+<li class="nav-item nav-link">
     <div class="dropdown">
         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
             Insert
@@ -7,6 +7,19 @@
             <a class="dropdown-item" href="{{ url('/authors/add') }}">Author</a>
             <a class="dropdown-item" href="{{ url('/booktypes/add') }}">Book Genre</a>
             <a class="dropdown-item" href="{{ url('/books/add') }}">Book</a>
+        </div>
+    </div>
+</li>
+
+<li class="nav-item nav-link">
+    <div class="dropdown">
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+            List
+        </button>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="{{ url('/authors') }}">Authors</a>
+            <a class="dropdown-item" href="{{ url('/booktypes') }}">Book Genres</a>
+            <a class="dropdown-item" href="{{ url('/books') }}">Books</a>
         </div>
     </div>
 </li>
