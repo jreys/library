79e3f4703afea22e3090445d910c12cb4f8a4541
João Reys Santos <je_reyssantos@msn.com>
2018-10-15 10:29:54 +0100

    Adding author and genre lists

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function authors() {
+        return Author::all();
+    }
+
+    public function types() {
+        return BookType::all();
+    }
 }
